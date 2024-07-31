1c11c6d4fa59a93e6891354255f78f7dcea0e0b2
Ninad <ninad16sawant@gmail.com>
2024-07-31 22:41:14 +0530

    media query added

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -16,135 +16,140 @@ final GlobalKey<FormState> formKey = GlobalKey<FormState>();
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         title: const Text("Home Screen"),
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 12),
-        child: Form(
-          key: formKey,
-          child: Consumer<ManageItemProvider>(builder: (context, state, child) {
-            return ListView(
-              physics: const NeverScrollableScrollPhysics(),
-              children: [
-                TextFormField(
-                  key: const Key("nameKey"),
-                  controller: state.name,
-                  inputFormatters: [
-                    LengthLimitingTextInputFormatter(100),
-                  ],
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return "Invalid field";
-                    }
-                    return null;
-                  },
-                  decoration: const InputDecoration(
-                    hintText: "Enter Name",
+      body: SizedBox(
+        width: size.width < 700 ? double.infinity : 500,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 12),
+          child: Form(
+            key: formKey,
+            child:
+                Consumer<ManageItemProvider>(builder: (context, state, child) {
+              return ListView(
+                physics: const NeverScrollableScrollPhysics(),
+                children: [
+                  TextFormField(
+                    key: const Key("nameKey"),
+                    controller: state.name,
+                    inputFormatters: [
+                      LengthLimitingTextInputFormatter(100),
+                    ],
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Invalid field";
+                      }
+                      return null;
+                    },
+                    decoration: const InputDecoration(
+                      hintText: "Enter Name",
+                    ),
                   ),
-                ),
-                const SizedBox(
-                  height: 8,
-                ),
-                TextFormField(
-                  key: const Key("descKey"),
-                  controller: state.desc,
-                  inputFormatters: [
-                    LengthLimitingTextInputFormatter(400),
-                  ],
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return "Invalid field";
-                    }
-                    return null;
-                  },
-                  decoration: const InputDecoration(
-                    hintText: "Enter Description",
+                  const SizedBox(
+                    height: 8,
                   ),
-                ),
-                const SizedBox(
-                  height: 8,
-                ),
-                MaterialButton(
-                  color: Colors.green,
-                  onPressed: () {
-                    state.updateId == null
-                        ? state.addToItemList()
-                        : state.editToItemList(state.updateId);
-                  },
-                  child: Text(
-                    state.updateId != null ? "Update Item" : "Add Item",
-                    style: const TextStyle(color: Colors.white),
+                  TextFormField(
+                    key: const Key("descKey"),
+                    controller: state.desc,
+                    inputFormatters: [
+                      LengthLimitingTextInputFormatter(400),
+                    ],
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Invalid field";
+                      }
+                      return null;
+                    },
+                    decoration: const InputDecoration(
+                      hintText: "Enter Description",
+                    ),
                   ),
-                ),
-                TextButton(
-                  style:
-                      const ButtonStyle(visualDensity: VisualDensity.compact),
-                  onPressed: state.onClear,
-                  child: const Text(
-                    "clear",
-                    style: TextStyle(color: Colors.red),
+                  const SizedBox(
+                    height: 8,
                   ),
-                ),
-                const Divider(),
-                ListView(
-                  shrinkWrap: true,
-                  children: state.itemData.map((e) {
-                    return Column(
-                      children: [
-                        Row(
-                          children: [
-                            Expanded(
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    e['name'].toString(),
-                                    style: const TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 14,
+                  MaterialButton(
+                    color: Colors.green,
+                    onPressed: () {
+                      state.updateId == null
+                          ? state.addToItemList()
+                          : state.editToItemList(state.updateId);
+                    },
+                    child: Text(
+                      state.updateId != null ? "Update Item" : "Add Item",
+                      style: const TextStyle(color: Colors.white),
+                    ),
+                  ),
+                  TextButton(
+                    style:
+                        const ButtonStyle(visualDensity: VisualDensity.compact),
+                    onPressed: state.onClear,
+                    child: const Text(
+                      "clear",
+                      style: TextStyle(color: Colors.red),
+                    ),
+                  ),
+                  const Divider(),
+                  ListView(
+                    shrinkWrap: true,
+                    children: state.itemData.map((e) {
+                      return Column(
+                        children: [
+                          Row(
+                            children: [
+                              Expanded(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      e['name'].toString(),
+                                      style: const TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 14,
+                                      ),
                                     ),
-                                  ),
-                                  Text(
-                                    e['desc'].toString(),
-                                    textAlign: TextAlign.justify,
-                                    style: const TextStyle(
-                                      fontWeight: FontWeight.normal,
-                                      fontSize: 12,
-                                    ),
-                                  )
-                                ],
+                                    Text(
+                                      e['desc'].toString(),
+                                      textAlign: TextAlign.justify,
+                                      style: const TextStyle(
+                                        fontWeight: FontWeight.normal,
+                                        fontSize: 12,
+                                      ),
+                                    )
+                                  ],
+                                ),
                               ),
-                            ),
-                            IconButton(
-                              onPressed: () {
-                                state.onEdit(e['id']);
-                              },
-                              icon: const Icon(
-                                Icons.edit,
-                                color: Colors.green,
+                              IconButton(
+                                onPressed: () {
+                                  state.onEdit(e['id']);
+                                },
+                                icon: const Icon(
+                                  Icons.edit,
+                                  color: Colors.green,
+                                ),
                               ),
-                            ),
-                            IconButton(
-                              onPressed: () {
-                                state.deleteItem(e['id']);
-                              },
-                              icon: const Icon(
-                                Icons.delete,
-                                color: Colors.red,
+                              IconButton(
+                                onPressed: () {
+                                  state.deleteItem(e['id']);
+                                },
+                                icon: const Icon(
+                                  Icons.delete,
+                                  color: Colors.red,
+                                ),
                               ),
-                            ),
-                          ],
-                        ),
-                        const Divider(),
-                      ],
-                    );
-                  }).toList(),
-                )
-              ],
-            );
-          }),
+                            ],
+                          ),
+                          const Divider(),
+                        ],
+                      );
+                    }).toList(),
+                  )
+                ],
+              );
+            }),
+          ),
         ),
       ),
     );
